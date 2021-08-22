afeda77f297034aa1157c768654ed579ced1e137
koshey <shdeemon@mail.ru>
2021-08-22 18:40:58 +0300

    Frontend: stylized & animated an opening image

--- src/frontend/src/app/idea-fill/idea-fill.component.ts
+++ src/frontend/src/app/idea-fill/idea-fill.component.ts
@@ -2,11 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Idea} from "../models/Idea";
 import {environment} from "../../environments/environment";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-idea-fill',
   templateUrl: './idea-fill.component.html',
-  styleUrls: ['./idea-fill.component.css']
+  styleUrls: ['./idea-fill.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ visibility: 'hidden', opacity: 0 }),
+        animate('.3s', style({ visibility: 'visible', opacity: 1 })),
+      ]),
+      transition('* => void', [
+        style({ visibility: 'visible', opacity: 1 }),
+        animate('.3s', style({ visibility: 'hidden', opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class IdeaFillComponent implements OnInit {
 
