98b2089b37ff8edc0652952490f5ad41a71b29d0
koshey <shdeemon@mail.ru>
2021-09-07 07:56:49 +0300

    Frontend: added filter to user-ideas

--- src/frontend/src/app/templates/user/ideas/user-ideas.component.ts
+++ src/frontend/src/app/templates/user/ideas/user-ideas.component.ts
@@ -12,12 +12,71 @@ export class UserIdeasComponent implements OnInit {
 
   public user: User;
   public ideas: Idea[];
+  public filteredIdeas: Idea[];
+
+  public reverse: boolean = false;
+  public status: string = "LOOKING";
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.user = this.activatedRoute.snapshot.data.user;
     this.ideas = this.user.ideas;
+    this.filteredIdeas = this.ideas
   }
 
+  sort(value: number) {
+    switch (value) {
+      case 1:
+        this.filteredIdeas.sort((a: Idea, b: Idea) => {
+          if(b.title.toLowerCase() < a.title.toLowerCase())
+            return 1;
+          else if(a.title.toLowerCase() < b.title.toLowerCase())
+            return -1;
+          else return 0;
+        });
+        break;
+      case 2:
+        this.filteredIdeas.sort((a, b) => {
+          if (b.looks > a.looks)
+            return 1
+          else if (b.looks < a.looks)
+            return -1
+          else return 0
+        });
+        break;
+      case 3:
+        this.filteredIdeas.sort((a, b) => {
+          if (b.rating > a.rating)
+            return 1
+          else if (b.rating < a.rating)
+            return -1
+          else return 0
+        });
+        break;
+      case 4:
+        this.filteredIdeas.sort((a, b) => {
+          if (b.createdDate > a.createdDate)
+            return 1
+          else if (b.createdDate < a.createdDate)
+            return -1
+          else return 0
+        });
+        break;
+    }
+
+    if(this.reverse)
+      this.filteredIdeas.reverse();
+  }
+
+  filter() {
+    if (this.status) {
+      this.filteredIdeas = this.ideas.filter((idea: Idea) => idea.status == this.status);
+    }
+  }
+
+  reverseSort() {
+    this.reverse = !this.reverse;
+    this.filteredIdeas.reverse();
+  }
 }
