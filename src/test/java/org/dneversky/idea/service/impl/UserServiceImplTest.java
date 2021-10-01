57e1b427e9f55ad52df6cbbb022936d196cbacd7
shdeemon <shdeemon@mail.ru>
2021-10-01 09:09:30 +0300

    Tests for user's methods:   getAll() ~100%   saveUser() ~80%   getUserByUsername() ~100%

--- src/test/java/org/dneversky/idea/service/impl/UserServiceImplTest.java
+++ src/test/java/org/dneversky/idea/service/impl/UserServiceImplTest.java
@@ -1,28 +1,32 @@
 package org.dneversky.idea.service.impl;
 
-import lombok.var;
 import org.dneversky.idea.entity.User;
 import org.dneversky.idea.repository.NotificationRepository;
 import org.dneversky.idea.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static reactor.core.publisher.Mono.when;
+import javax.persistence.EntityExistsException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class UserServiceImplTest {
 
-    @Mock
+    private final User USER = new User("e@e", "123");
+
+    @Spy
     private UserRepository userRepository;
     @Mock
     private NotificationRepository notificationRepository;
@@ -35,40 +39,63 @@ class UserServiceImplTest {
 
     @Test
     void getAllUsers() {
+        given(userRepository.findAll()).willReturn(new ArrayList<>());
+
         userService.getAllUsers();
 
-        verify(userRepository).findAll();
+        then(userRepository).should().findAll();
     }
 
     @Test
+    @Disabled
     void getUser() {
     }
 
     @Test
     void getUserByUsername() {
+        // given
+        given(userRepository.findByUsername(USER.getUsername())).willReturn(Optional.of(USER));
+
+        // when
+        userService.getUserByUsername(USER.getUsername());
+
+        // then
+        then(userRepository).should().findByUsername(USER.getUsername());
     }
 
     @Test
     void saveUser() {
         // given
-        given(userRepository.getByUsername("e@e")).willReturn(new User("e@e", "123"));
+        given(userRepository.save(USER)).willReturn(USER);
 
         // when
-        var user = userService.getUserByUsername("e@e");
+        userService.saveUser(USER);
 
-        assertThat(user).isNotNull();
+        // then
+        then(userRepository).should().save(USER);
     }
 
     @Test
-    void saveUserIfUserExists() {
-        User user = new User();
+    @Disabled
+    void saveExistsUser() {
+        given(userRepository.findByUsername(USER.getUsername())).willReturn(Optional.of(USER));
+        willThrow(new EntityExistsException()).given(userRepository).save(USER);
+
+        try {
+            userService.saveUser(USER);
+            fail("Should throw exception");
+        } catch (EntityExistsException ignored) {}
+
+        then(userRepository).should(never()).save(USER);
     }
 
     @Test
+    @Disabled
     void updateUser() {
     }
 
     @Test
+    @Disabled
     void deleteUser() {
     }
 }
