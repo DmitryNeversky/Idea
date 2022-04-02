2265fa66c643f57aabcaff5369ecc7cb12e0738d
koshey <shdeemon@mail.ru>
2022-04-02 14:56:18 +0300

    added my thoughts

--- backend/chatter/src/main/java/com/dneversky/chatter/config/WebSocketConfig.java
+++ backend/chatter/src/main/java/com/dneversky/chatter/config/WebSocketConfig.java
@@ -2,10 +2,17 @@ package com.dneversky.chatter.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -18,12 +25,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
                 .setAllowedOrigins(allowedOrigin)
+                /* TODO: JWT identification
+                .addInterceptors(new HandshakeInterceptor() {
+                    @Override
+                    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                        return request.getHeaders().get("Authorization") != null; // Уязвимость
+                    }
+
+                    @Override
+                    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+                    }
+                }) */
                 .withSockJS();
+                registry.setErrorHandler(new StompSubProtocolErrorHandler());
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app")
-                .enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/api")
+                .enableSimpleBroker("/topic", "/queue");
     }
 }
