f65c621bd7df9de854e5bf2340055516b677ddc4
Koshey <shdeemon@mail.ru>
2022-07-26 08:45:32 +0300

    refactored
    Signed-off-by: Koshey <shdeemon@mail.ru>

--- backend/gateway/src/main/java/org/dneversky/gateway/advice/UserAdvice.java
+++ backend/gateway/src/main/java/org/dneversky/gateway/advice/UserAdvice.java
@@ -1,8 +1,8 @@
 package org.dneversky.gateway.advice;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.dneversky.gateway.advice.annotation.PrincipalOrAdminAccess;
@@ -11,11 +11,12 @@ import org.dneversky.gateway.exception.UnforeseenException;
 import org.dneversky.gateway.service.impl.DefaultUserService;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Aspect
 @Component
@@ -30,16 +31,21 @@ public class UserAdvice {
     @Pointcut("@annotation(org.dneversky.gateway.advice.annotation.PrincipalOrAdminAccess)")
     private void annotatedMethodsWithPrincipalOrAdminAccess() {}
 
-    @After("annotatedMethodsWithPrincipalOrAdminAccess()")
-    public void beforeCurrentUserMethod(JoinPoint point) {
+    // check if current user has the same username as incoming and certain roles
+
+    @Before("annotatedMethodsWithPrincipalOrAdminAccess()")
+    public void providePrincipalOrAdminAccess(JoinPoint point) {
         MethodSignature methodSignature = (MethodSignature) point.getSignature();
         PrincipalOrAdminAccess annotation = methodSignature.getMethod().getAnnotation(PrincipalOrAdminAccess.class);
-        String targetParameterName = annotation.target();
+        String methodUsernameParameterName = annotation.target();
         String[] parameterNames = methodSignature.getParameterNames();
+
+        // checking parameter existing that had been specified in the annotation's
+        // parameter 'target' by comparing indexes of incoming arguments and method parameters
 
         String targetUsername = null;
         for(int i = 0; i < parameterNames.length; i++) {
-            if(parameterNames[i].equals(targetParameterName)) {
+            if(parameterNames[i].equals(methodUsernameParameterName)) {
                 targetUsername = point.getArgs()[i].toString();
                 break;
             }
@@ -47,10 +53,13 @@ public class UserAdvice {
         if(targetUsername == null) {
             return;
         }
-
         Authentication authentication = getAuthentication();
         long foundRolesQuantity = Arrays.stream(annotation.roles())
-                .filter(e -> authentication.getAuthorities().contains(new SimpleGrantedAuthority(e)))
+                .filter(e -> authentication.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toSet())
+                        .contains(e))
                 .count();
         if(!targetUsername.equals(authentication.getName()) && foundRolesQuantity == 0) {
             throw new PermissionException("Not enough privileges.");
