ffae067b24b873d8a69b9e14067508970b62a53f
Koshey <shdeemon@mail.ru>
2022-09-19 10:16:56 +0300

    refactored
    Signed-off-by: Koshey <shdeemon@mail.ru>

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: "http://213.219.212.27:8080",
-  apiBaseUrl: "http://213.219.212.27:8080/api",
+  serverUrl: "http://45.132.1.86:8080",
+  apiBaseUrl: "http://45.132.1.86:8080/api",
   uploadPath: "assets/uploaded/"
 };
